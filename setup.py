2a79bec9075eda756ecc71047ac0dc9193eee814
bartvanb <b.vanbeusekom@iknl.nl>
2025-07-29 12:49:44 +0200

    Update vantage6 version

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from os import path
 from codecs import open
-from setuptools import setup, find_packages
+from os import path
+
+from setuptools import find_packages, setup
 
 # we're using a README.md, if you do not have this in your folder, simply
 # replace this with a string.
@@ -19,5 +20,5 @@ setup(
     url="https://github.com/vantage6/v6-session-basics",
     packages=find_packages(),
     python_requires=">=3.10",
-    install_requires=["vantage6-algorithm-tools==5.0.0a19", "pandas"],
+    install_requires=["vantage6-algorithm-tools==5.0.0a26", "pandas"],
 )
